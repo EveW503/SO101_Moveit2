c5f47fb8e803631b1c164d980b4827abd92ba137
EveW503 <2453124@tongji.edu.cn>
2026-03-25 11:02:49 +0800

    SO101 in moveit2

---
+++ build/so101_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ljq/so101_ws/src/so101_moveit_config
+# Build directory: /home/ljq/so101_ws/build/so101_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
